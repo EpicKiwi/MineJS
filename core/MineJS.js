21baa69f262284507a8f08b14455fa020ea8b5e6
EpicKiwi <lelycandu76@gmail.com>
2015-05-04 13:58:13 +0200

    Detection basique de l'etat d'installation

--- core/MineJS.js
+++ core/MineJS.js
@@ -3,14 +3,26 @@ var expressApp = express();
 var http = require('http').Server(expressApp);
 var io = require('socket.io')(http);
 
+var log = require("./Logger");
+var setupManager = require("./SetupManager.js");
+
 expressApp.use("/static",express.static(__dirname+"/static"));
 
 expressApp.get("/",function(request,response){
 	response.send("Hello world");
 });
 
+expressApp.get("/debug",function(request, response){
+	response.send(JSON.stringify(setupManager.checklist));
+});
+
 function init(){
-	http.listen(80);
+	log.info("Verification de l'installation");
+	setupManager.check();
+
+	http.listen(80,function(){
+		log.info("Ecoute sur *:80");
+	});
 };
 
 exports.init = init;
---
+++ core/SetupManager.js
@@ -0,0 +1,107 @@
+var fs = require("fs");
+var log = require("./Logger");
+
+var installChecklist = {
+	config     : false,
+	users      : false,
+	gameServer : false,
+};
+
+function checkSetup()
+{
+	createFileSystem();
+	installChecklist.config = checkFile(__dirname+"/../config/config.yml");
+	installChecklist.users = checkUsers();
+};
+
+function checkUsers()
+{
+	try
+	{
+		var users = fs.readdirSync(__dirname+"/../config/users");
+		if(users.length > 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	catch(e)
+	{
+		if(e.code == "ENOENT")
+		{
+			log.warn("Le dossier utilisateurs n'existe pas");
+			return false;
+		}
+		else
+		{
+			console.trace(e);
+		}
+	}
+}
+
+function checkFile(path)
+{
+	try
+	{
+		fs.accessSync(path);
+	}
+	catch(e)
+	{
+		if(e.code == "ENOENT")
+		{
+			return false;
+		}
+		else
+		{
+			console.trace(e);
+		}
+	}
+	return true;
+}
+
+function createFileSystem()
+{
+	checkFolder(__dirname+"/../config");
+	checkFolder(__dirname+"/../config/users");
+	checkFolder(__dirname+"/../gamefiles");
+	checkFolder(__dirname+"/../apps");
+};
+
+function checkFolder(path)
+{
+	try
+	{
+		fs.accessSync(path);
+	}
+	catch(e)
+	{
+		if(e.code == "ENOENT")
+		{
+			log.warn("Le dossier "+path+" n'existe pas. Création ...");
+			fs.mkdirSync(path);
+		}
+		else
+		{
+			console.trace(e);
+		}
+	}
+}
+
+function isCompletlyInstalled()
+{
+	for(var elem in installChecklist)
+	{
+		if(!installChecklist[elem])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+exports.isCompletlyInstalled = isCompletlyInstalled;
+exports.checklist = installChecklist;
+exports.check = checkSetup;
