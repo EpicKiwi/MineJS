3a0f9d0cebdaed2f89986562d52e2104a0ee7967
EpicKiwi <lelycandu76@gmail.com>
2015-05-04 15:06:22 +0200

    Recherche d'un executable

--- core/MinecraftServer.js
+++ core/MinecraftServer.js
@@ -1,8 +1,12 @@
 var cp = require("child_process");
+var fs = require("fs");
 
 var MineJS = require("./MineJS");
+var log = require("./Logger");
 
 var folder = "";
+var executable = null;
+var version = null;
 
 function getAbsolutePath()
 {
@@ -12,7 +16,46 @@ function getAbsolutePath()
 function init()
 {
 	folder = MineJS.getConfig().gameServerFolder;
+	searchExecutable();
+	log.log("serveur minecraft : version = "+version+" / executable = "+executable);
+}
+
+function searchExecutable()
+{
+	try
+	{
+		var files = fs.readdirSync(getAbsolutePath());
+	}
+	catch(e)
+	{
+		if(e.code == "ENOENT")
+		{
+			log.error("Le dossier "+folder+" n'exste pas");
+		}
+		else
+		{
+			console.trace(e);
+		}
+		return false;
+	}
+
+	for(var i = 0; i<files.length; i++)
+	{
+		if(files[i].search(/minecraft_server\.(.*)\.(exe|jar)/i) > -1)
+		{
+			executable = files[i];
+			version = files[i].replace(/minecraft_server\.(.*)\.(exe|jar)/i,"$1");
+			return true;
+		}
+	}
+
+	log.error("Aucun executable de serveur trouvé");
+
 }
 
 exports.getPath = getAbsolutePath;
 exports.init = init;
+
+exports.getVersion = function(){
+	return version;
+};
