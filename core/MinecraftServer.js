23a1f31a0e43f380cae0f32f05dcb46358e5f15f
EpicKiwi <lelycandu76@gmail.com>
2015-05-04 16:19:54 +0200

    Emission d'evenements

--- core/MinecraftServer.js
+++ core/MinecraftServer.js
@@ -1,5 +1,6 @@
 var cp = require("child_process");
 var fs = require("fs");
+var events = require('events');
 
 var MineJS = require("./MineJS");
 var log = require("./Logger");
@@ -10,6 +11,8 @@ var version = null;
 var ram = 2048;
 var process = null;
 var running = false;
+var emitter = new events.EventEmitter();
+var lastCommand = null;
 
 function getAbsolutePath()
 {
@@ -20,6 +23,7 @@ function init()
 {
 	folder = MineJS.getConfig().gameServerFolder;
 	searchExecutable();
+	eventDispatcher();
 	if(MineJS.getConfig().gameServerAutoStart)
 	{
 		run();
@@ -61,7 +65,8 @@ function searchExecutable()
 
 function run()
 {
-	log.info("Démarrage du serveur minecraft");
+	log.info("Minecraft : Démarrage du serveur");
+	emitter.emit("load");
 	running = true;
 	process = cp.spawn("java",["-Xmx"+ram+"M","-Xms"+ram+"M","-jar",executable,"nogui"],{cwd:getAbsolutePath()});
 	process.stdout.setEncoding("UTF-8");
@@ -72,14 +77,15 @@ function run()
 		if(data.search(/\r\n/i) != -1)
 		{
 			line = line.replace(/(.*)\r\n/i,"$1");
-			log.log("Minecraft : "+line);
+			analyzeLine(line);
 			line = "";
 		}
 	});
 
 	process.on("close",function(){
 		running = false;
-		log.info("Serveur Minecraft étein");
+		emitter.emit("close");
+		log.info("Minecraft : Serveur etein");
 	});
 }
 
@@ -87,18 +93,55 @@ function run()
 function sendCommand(command){
 	if(running)
 	{
+		lastCommand = command;
 		process.stdin.write(command+"\n");
-		log.info("Commande "+command+" envoyée au serveur");
+		log.info("Minecraft : Commande "+command);
 	}
 	else
 	{
-		log.error("La commande "+command+" ne peut etre envoyée car le serveur n'est pas démarré");
+		log.error("Minecraft : La commande "+command+" ne peut etre envoyé quand le serveur est étein");
 	}
 };
+
+function analyzeLine(line)
+{
+	line = line.replace(/\[..\:..\:..\] \[.*\/(.*)\].*: (.*)/i,"$1#$2").split("#");
+	emitter.emit("log",line[1]);
+	switch(line[0])
+	{
+		case "INFO":
+			emitter.emit("info",line[1]);
+		break;
+		case "WARN":
+			log.warn("Minecraft : "+line[1]);
+			emitter.emit("warning",line[1]);
+		break;
+		case "ERROR":
+			log.error("Minecraft : "+line[1]);
+			emitter.emit("error",line[1]);
+		break;
+	}
+}
+
+function eventDispatcher(){
+	emitter.on("info",function(message){
+		if(message.search(/Done \(.*\)/i) != -1)
+		{
+			log.info("Minecraft : Serveur pret");
+			emitter.emit("ready");
+		}
+		else if(message.search(/Unknown command/i) != -1)
+		{
+			log.error("Minecraft : Commande "+lastCommand+" inconnue");
+			emitter.emit("badCommand",lastCommand);
+		}
+	});
+}
 
 exports.getPath = getAbsolutePath;
 exports.init = init;
 exports.run = run;
+exports.sendCommand = sendCommand;
 
 exports.getVersion = function(){
 	return version;
@@ -106,4 +149,8 @@ exports.getVersion = function(){
 
 exports.isRunning = function(){
 	return running;
+}
+
+exports.getEmitter = function(){
+	return emitter;
 }
